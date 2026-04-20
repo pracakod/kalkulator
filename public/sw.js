a8c30cf9f17c3e7c1a1a67afe39da88cc4c0daff
pracakod <praca.kod@gmail.com>
2026-04-20 20:09:40 +0200

    Update SW to v2 to force refresh and fix 404 icons

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,26 @@
-const CACHE_NAME = 'kalkulator-v1';
+const CACHE_NAME = 'kalkulator-v2'; // Zmiana wersji wymusza odświeżenie
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.svg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
