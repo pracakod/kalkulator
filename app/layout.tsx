cc09eab83dd1cf522175f2ba286241848e6b30a9
pracakod <praca.kod@gmail.com>
2026-04-20 20:01:41 +0200

    Correct Viewport metadata and import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-import type {Metadata} from 'next';
+import type {Metadata, Viewport} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Kalkulator Godzin',
   description: 'Oblicz ilość czasu między dwoma określonymi godzinami.',
   manifest: '/manifest.json',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#2563eb',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
